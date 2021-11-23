701be455f3d557ce5e966cde547344eb0013b9f3
donlayao <dexter@netlinkvoice.com>
2021-11-23 13:32:48 +0800

    LARAVEL NUXT

---
+++ client/pages/auth/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <form action="" @submit.prevent="handleLogin">
+      <input type="text" v-model="form.email" />
+      <input type="text" v-model="form.password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {},
+    };
+  },
+  methods: {
+    handleLogin() {
+      this.$auth
+        .loginWith("laravelJWT", {
+          data: this.form,
+        })
+        .then(() => {
+          this.$router.push("/dashboard");
+        });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ client/pages/dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <button @click="handleCallTestAPI">Call TEST API</button>
+    <button @click="handleLogout">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleLogout() {
+      this.$auth.logout().then(() => {
+        this.$router.push("auth/login");
+      });
+    },
+    handleCallTestAPI() {
+      this.$axios
+        .get("/api/test")
+        .then((response) => console.log(response.data));
+    },
+  },
+};
+</script>
+
+<style>
+</style>
